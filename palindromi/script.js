3cb4fd8d0f8adce94cf4345f8c746cfb57810da0
Nino Chimento <saninoc84@gmail.com>
2020-01-13 16:17:13 +0100

    funziona creo var da utente e poi funzione

--- palindromi/script.js
+++ palindromi/script.js
@@ -1,7 +1,8 @@
 
-var parola = "ovattavb"
+var parola = "ovattavb";
 var listaParola = [];
 var listaParolaDaCapovolgere = [];
+var conteggio=0;
 for (var i = 0; i < parola.length; i++) {
   listaParola.push(parola[i])
 }
@@ -9,6 +10,18 @@ for (var i = 0; i < parola.length; i++) {
 for (var i = 0; i < parola.length; i++) {
   listaParolaDaCapovolgere.push(parola[i])
 }
-var reversed = listaParolaDaCapovolgere.reverse()
+var reversed = listaParolaDaCapovolgere.reverse();
+for (var i = 0; i < listaParola.length; i++) {
+  if (listaParola[i] == listaParolaDaCapovolgere[i]) {
+    conteggio++
+  }
+}
+if (conteggio == listaParola.length) {
+  alert("le parole sono palindrome")
+}
+else {
+  alert("le parole non sono palindrome")
+}
 console.log(listaParola);
 console.log(listaParolaDaCapovolgere);
+console.log(conteggio);
