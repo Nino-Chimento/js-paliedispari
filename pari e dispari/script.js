2a74b447f2059ae6e60bfc3794de30f71370015e
Nino Chimento <saninoc84@gmail.com>
2020-01-13 12:30:17 +0100

    calcolo numero totale se pari o dispari con funzione

---
+++ pari e dispari/script.js
@@ -0,0 +1,25 @@
+// L’utente sceglie pari o dispari e un numero da 1 a 5.
+var numeroUtente = parseInt(prompt("inserisci un numero"));
+var scommessa = prompt(" scegli pari o dispari");
+// Generiamo un numero random (sempre da 1
+// a 5) per il computer.
+numeroPc =getRandomIntInclusive(1,5);
+
+// Sommiamo i due numeri e dichiariamo chi ha
+// vinto (controlliamo se la somma da pari o dispari).
+var totale = isEven(numeroUtente + numeroPc);
+console.log(totale);
+// funzioni da utilizzare
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function isEven(num){
+  if (num%2 == 0) {
+    return true
+  }
+  else{
+    return false
+  }
+}
