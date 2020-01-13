d04b3df82ecd8b942e80ffeb46d7113ae9ec86fb
Nino Chimento <saninoc84@gmail.com>
2020-01-13 13:17:13 +0100

    creazione array con lettere di ogni parola

--- palindromi/script.js
+++ palindromi/script.js
@@ -1,6 +1,16 @@
 
 var primaParola = "anna"
+var listaPrimaParola =[]
 var secondaParola = "anna"
+var listaSecondaParola =[]
 if (primaParola.length != secondaParola.lenght) {
     alert("le parole non hanno la stessa lunghezza")
 }
+for (var i = 0; i < primaParola.length; i++) {
+  listaPrimaParola.push(primaParola[i]);
+}
+console.log(listaPrimaParola);
+for (var i = 0; i < secondaParola.length; i++) {
+  listaSecondaParola.push(secondaParola[i]);
+}
+console.log(listaSecondaParola);
