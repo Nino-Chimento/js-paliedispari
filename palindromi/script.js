0e243a6fb5e503cc08cec0c759cbb1bb745f4465
Nino Chimento <saninoc84@gmail.com>
2020-01-13 13:31:56 +0100

    creazioen array della parola e creazione arrey reversed

--- palindromi/script.js
+++ palindromi/script.js
@@ -1,2 +1,9 @@
 
 var parola = "anna"
+var listaParola = [];
+for (var i = 0; i < parola.length; i++) {
+  listaParola.push(parola[i])
+}
+console.log(listaParola);
+var reversed = listaParola.reverse();
+console.log(reversed);
